4f1a715b1caa1ce107b34141e4c366415dc4cd44
Nico Hämäläinen <nico@sizeof.io>
2016-09-21 19:44:00 +0300

    Added tasks resource

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,15 @@
 'use strict';
+const task = require('./task');
 const authentication = require('./authentication');
 const user = require('./user');
 const mongoose = require('mongoose');
 module.exports = function() {
   const app = this;
-  
+
   mongoose.connect(app.get('mongodb'));
   mongoose.Promise = global.Promise;
-  
+
   app.configure(authentication);
   app.configure(user);
+  app.configure(task);
 };
